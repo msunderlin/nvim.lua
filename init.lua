f095a752e9cfcd38bf6218cd54c137cb9c5a4cf9
Michael Sunderlin <msunderlin@gmail.com>
2022-10-19 13:56:30 -0700

    Working through lsp setup for nvim

--- init.lua
+++ init.lua
@@ -55,3 +55,10 @@ require('lualine').setup {
     -- ... your lualine config
   }
  }
+require'lspconfig'.phpactor.setup{
+    on_attach = on_attach,
+    init_options = {
+        ["language_server_phpstan.enabled"] = false,
+        ["language_server_psalm.enabled"] = false,
+    }
+}
